1da5e6994b6a576b5c4efa28b5999eb9b599ec37
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-08-18 19:29:33 +0530

    Update var.tf
    Removed task variable

--- var.tf
+++ var.tf
@@ -6,11 +6,6 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be created"
 }
 
-variable "task1q" {
-  description = "The Azure Region in which all resources in this example should be created"
-}
-
-
 variable "subscriptionId" {}
 variable "clientId" {}
 variable "clientSecret" {}
