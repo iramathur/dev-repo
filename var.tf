064c3ff953895bac1ef26a4da601ff2c5136e802
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-08-18 19:14:48 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -6,7 +6,7 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be created"
 }
 
-variable "task1" {
+variable "task1q" {
   description = "The Azure Region in which all resources in this example should be created"
 }
 
