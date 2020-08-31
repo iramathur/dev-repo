149abb0a90eddd1b19854b86188fa468679295e9
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-08-31 18:40:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,9 @@
 provider "azurerm" {
-  environment     = "usgovernment"
   subscription_id = "${var.subscriptionId}"
   client_id       = "${var.clientId}"
   client_secret   = "${var.clientSecret}"
   tenant_id       = "${var.tenantId}"
-  version = "=1.44.0"
+#  features {}
+   version = "=1.44.0"
 }
-
 
